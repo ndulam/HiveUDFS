e7d2e517b4352ad71c11937ac462745ee3ed35b4
NDhulam <naresh.this@gmail.com>
2016-07-09 23:55:29 -0700

    Finding Indexes

---
+++ src/com/naresh/org/udf/FindingIndex.java
@@ -0,0 +1,25 @@
+package com.naresh.org.udf;
+
+
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+import java.util.ArrayList;
+
+/**
+ * Given an array of booleans, returns the indices of the TRUE values.
+ */
+@Description(name = "udfwhich",
+             value = "_FUNC_(values) - Given an array of booleans, returns the (0-indexed) indices of the TRUE values.")
+
+  public class FindingIndex extends UDF {
+    public ArrayList<Integer> evaluate(Boolean... vals) {
+      ArrayList<Integer> result = new ArrayList<Integer>();
+      for (int ii = 0; ii < vals.length; ++ii) {
+        if (vals[ii]) {
+          result.add(ii);
+        }
+      }
+      return result;
+    }
+  }
